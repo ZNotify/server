5480b783c97515a900d0e637045a06ee5b32938f
Zxilly <zhouxinyu1001@gmail.com>
2022-10-23 00:05:35 +0800

    feat: add duration middleware
    [deploy]
    
    Signed-off-by: Zxilly <zhouxinyu1001@gmail.com>

--- serve/middleware/duration.go
+++ serve/middleware/duration.go
@@ -1,14 +1,24 @@
 package middleware
 
 import (
+	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
+type DurationWriter struct {
+	gin.ResponseWriter
+	startTime time.Time
+}
+
+func (w DurationWriter) WriteHeader(code int) {
+	w.Header().Set("X-Response-Time", strconv.FormatInt(time.Since(w.startTime).Microseconds(), 10))
+	w.ResponseWriter.WriteHeader(code)
+}
+
 func Duration(c *gin.Context) {
 	currentTime := time.Now()
+	c.Writer = &DurationWriter{c.Writer, currentTime}
 	c.Next()
-	duration := time.Since(currentTime)
-	c.Writer.Header().Set("X-Duration", duration.String())
 }
