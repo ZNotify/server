7f87ecde509f816848f49aa8a0936623b9ad702b
Zxilly <zhouxinyu1001@gmail.com>
2022-12-12 23:40:39 +0800

    feat: add mock log
    Signed-off-by: Zxilly <zhouxinyu1001@gmail.com>

--- push/test_push.go
+++ push/test_push.go
@@ -2,9 +2,22 @@
 
 package push
 
-import pushTypes "notify-api/push/types"
+import (
+	"go.uber.org/zap"
+
+	pushTypes "notify-api/push/types"
+)
 
 func Send(msg *pushTypes.Message) error {
+	fields := []zap.Field{
+		zap.String("user_id", msg.UserID),
+		zap.String("title", msg.Title),
+		zap.String("content", msg.Content),
+		zap.String("long_content", msg.Long),
+		zap.String("priority", string(msg.Priority)),
+	}
+
+	zap.L().Info("Try to send message", fields...)
 	return nil
 }
 
